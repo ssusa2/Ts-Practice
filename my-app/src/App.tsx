bc5cb5720e5e2bdd3efed6cd48002569ceb2b6cc
ssusa2 <audgns9207@naver.com>
2022-07-11 22:47:52 +0900

    ts 기본 타입 연습

---
+++ my-app/src/App.tsx
@@ -0,0 +1,17 @@
+/** @format */
+
+import React from 'react';
+import logo from './logo.svg';
+import Modi from './Modi';
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			나는 APP 컴포넌트
+			<Modi />
+		</div>
+	);
+}
+
+export default App;
